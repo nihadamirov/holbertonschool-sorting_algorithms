978410ecc869269f7c4fc9fbcd8006414a2b5d66
Nihad <nihadamirov98@gmail.com>
2024-04-14 19:35:58 +0400

    Add file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - short description
+ *
+ * Description: long description
+ *
+ * @list: argument_1 description
+ *
+ * Return: return description
+ */
+void insertion_sort_list(listint_t **list)
+{
+    //TODO
+}
