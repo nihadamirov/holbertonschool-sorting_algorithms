df8718b4fc50033e58892902623d1e88ad73ba3d
Nihad <nihadamirov98@gmail.com>
2024-04-20 12:55:31 +0000

    update file

--- sort.h
+++ sort.h
@@ -31,7 +31,7 @@ void quick_sort(int *array, size_t size);
 /* quick_sort() helpers */
 int lomuto_partition(int *array, int low, int high, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
-void swap(int *a, int *b);
+void swap(int *a, int *b, int *array, size_t size);
 
 
 #endif
